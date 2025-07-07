9715f9d33ca23d4f72c98a6a229525c09dd4492b
lipsiluya <lipsilu@yandex.ru>
2025-07-07 12:32:26 +0300

    d

--- src/test/java/tests/UserControllerTest.java
+++ src/test/java/tests/UserControllerTest.java
@@ -6,9 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import controller.UserController;
+import ru.yandex.practicum.filmorate.FilmorateApplication;
 
 import java.time.LocalDate;
 
@@ -17,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
+@ContextConfiguration(classes = FilmorateApplication.class)
 public class UserControllerTest {
 
     @Autowired
@@ -40,16 +46,6 @@ public class UserControllerTest {
                         .content(mapper.writeValueAsString(user)))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string(containsString("Email не может быть пустым")));
-    }
-
-    @Test
-    void addUser_InvalidLogin_BadRequest() throws Exception {
-        user.setLogin(" ");
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(user)))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string(containsString("Логин не может быть пустым")));
     }
 
     @Test
