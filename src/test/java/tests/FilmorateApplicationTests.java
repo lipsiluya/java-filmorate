7b21c0ea08038ee85812f5ccbc76f1ab09c32ae3
lipsiluya <lipsilu@yandex.ru>
2025-07-07 15:26:22 +0300

    f

--- src/test/java/tests/FilmorateApplicationTests.java
+++ src/test/java/tests/FilmorateApplicationTests.java
@@ -2,8 +2,9 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.FilmorateApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = FilmorateApplication.class)
 class FilmorateApplicationTests {
 
     @Test
