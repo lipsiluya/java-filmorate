93bc35ae9d996e2c551ae64e8417e8333f4a2e7b
lipsiluya <lipsilu@yandex.ru>
2025-07-08 15:19:13 +0300

    f

--- src/main/java/com/example/controller/FilmorateApplication.java
+++ src/main/java/com/example/controller/FilmorateApplication.java
@@ -1,11 +1,22 @@
 package com.example.controller;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example.controller", "exception", "model"})
+@SpringBootApplication(scanBasePackages = {
+        "com.example.controller",
+        "com.example.model",
+        "com.example.exception"
+})
 public class FilmorateApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println(">>> >>> >>> Filmorate STARTED <<< <<< <<<");
     }
 }
