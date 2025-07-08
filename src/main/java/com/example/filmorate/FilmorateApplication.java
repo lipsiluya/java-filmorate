2fcc4f2117a7858de1de3057889af368c91f51dc
lipsiluya <lipsilu@yandex.ru>
2025-07-08 14:41:59 +0300

    g

--- src/main/java/com/example/filmorate/FilmorateApplication.java
+++ src/main/java/com/example/filmorate/FilmorateApplication.java
@@ -1,10 +1,10 @@
-package ru.yandex.practicum.filmorate.model;
+package com.example.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class}) // отключает автонастройку R2DBC
+@SpringBootApplication(scanBasePackages = {"com.example.filmorate", "exception", "model"})
 public class FilmorateApplication {
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
