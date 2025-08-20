7cafb533469e3930a6119e4fc7335ecc4c7530d4
Maxim Telegin <maximtelegin@MacBook-Pro-Maxim.local>
2025-08-20 14:27:05 +0300

    d

--- src/main/java/com/example/storage/UserStorage.java
+++ src/main/java/com/example/storage/UserStorage.java
@@ -1,11 +1,15 @@
 package com.example.storage;
 
 import com.example.model.User;
+
 import java.util.Collection;
 
 public interface UserStorage {
     User add(User user);
+
     User update(User user);
+
     User getById(Long id);
+
     Collection<User> getAll();
 }
