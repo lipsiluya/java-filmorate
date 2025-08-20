56c1d994f104047ad387c18dc2510b41dbc1a678
Maxim Telegin <maximtelegin@MacBook-Pro-Maxim.local>
2025-08-20 14:27:05 +0300

    d

--- src/main/java/com/example/storage/FilmStorage.java
+++ src/main/java/com/example/storage/FilmStorage.java
@@ -1,11 +1,15 @@
 package com.example.storage;
 
 import com.example.model.Film;
+
 import java.util.Collection;
 
 public interface FilmStorage {
     Film add(Film film);
+
     Film update(Film film);
+
     Film getById(int id);
+
     Collection<Film> getAll();
 }
